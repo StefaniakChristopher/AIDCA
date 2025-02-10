27129bd611a6e5620955163f97109b3a9e9994d3
savannahmoss <savannahmoss@oakland.edu>
2025-02-10 13:20:37 -0500

    DB connection test and test functions yayaya it works

---
+++ dist/db.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, 
+});
+
+pool.connect()
+  .then(() => console.log("Connected to Neon PostgreSQL!"))
+  .catch(err => console.error("Connection error", err));
+
+module.exports = pool;
