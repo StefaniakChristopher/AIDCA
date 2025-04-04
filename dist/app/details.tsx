d06dd0161ffa20ee1ab6e7d8e02f7abdd350ddd2
Chris <stefaniakchristopher@gmail.com>
2025-04-04 13:24:09 -0400

    formatted and fixed text string thing

--- dist/app/details.tsx
+++ dist/app/details.tsx
@@ -1,5 +1,13 @@
 import React, { useRef, useEffect } from "react";
-import { View, Text, Image, TouchableOpacity, Dimensions, Animated, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+  Animated,
+  SafeAreaView,
+} from "react-native";
 import ViewShot from "react-native-view-shot"; // For capturing and sharing
 import Feather from "@expo/vector-icons/Feather"; // For the share icon
 import Donut from "@/components/ui/Donut"; // Correct import for Donut component
@@ -15,11 +23,15 @@ const ImageDetailsScreen = () => {
       key: string;
       name: string;
       path?: string;
-      imageData: { file_path: string; file_path_heatmap: string; confidence_score: number };
+      imageData: {
+        file_path: string;
+        file_path_heatmap: string;
+        confidence_score: number;
+      };
     };
   };
 
-  const route = useRoute<RouteProp<ImageDetailsRouteParams, 'params'>>(); // Access route params
+  const route = useRoute<RouteProp<ImageDetailsRouteParams, "params">>(); // Access route params
   const navigation = useNavigation(); // For navigation
   const { imageData } = route.params || {}; // Destructure image data from route params
 
@@ -44,17 +56,17 @@ const ImageDetailsScreen = () => {
 
   // Function to capture and share the image
   const captureAndShare = async () => {
-      try {
-        const uri = await viewShotRef.current?.capture?.();
-        if (uri) {
-          await Sharing.shareAsync(uri);
-        } else {
-          console.warn("Capture failed or capture method is undefined.");
-        }
-      } catch (error) {
-        console.error("Error capturing or sharing image:", error);
+    try {
+      const uri = await viewShotRef.current?.capture?.();
+      if (uri) {
+        await Sharing.shareAsync(uri);
+      } else {
+        console.warn("Capture failed or capture method is undefined.");
       }
-    };
+    } catch (error) {
+      console.error("Error capturing or sharing image:", error);
+    }
+  };
 
   // Start animations when the component mounts
   useEffect(() => {
@@ -74,85 +86,106 @@ const ImageDetailsScreen = () => {
   return (
     <SafeAreaView className="flex-1 bg-backgroundPrimary">
       <View className="flex flex-row justify-between items-center px-6 pt-6">
-      <TouchableOpacity onPress={() => navigation.goBack()} className="pr-3">
-        <Text>
+        <TouchableOpacity onPress={() => navigation.goBack()} className="pr-3">
           <Ionicons name="arrow-back" size={28} color="#FFFFFF" />
-        </Text>
-      </TouchableOpacity>
-        <Text style={{ fontFamily: 'ClimateCrisis', fontSize: 32, color: '#21FA90' }}>
+        </TouchableOpacity>
+        <Text
+          style={{
+            fontFamily: "ClimateCrisis",
+            fontSize: 32,
+            color: "#21FA90",
+          }}
+        >
           Details
         </Text>
-        <View style={{ width: 28 }} /> Placeholder for alignment
+        <View style={{ width: 28 }} />
       </View>
 
       {/* Content */}
       <View className="flex-1">
-          <Animated.View style={{ opacity: fadeAnim, transform: [{ translateY: slideAnim }], marginTop: 20, }}>
-            <ViewShot ref={viewShotRef} options={{ format: "png", quality: 0.9 }}>
-              <View
-                className={`flex flex-col items-center justify-between p-4 rounded-lg ${
-                  aiPercent > 0.5 ? "bg-fontColorPrimary" : "bg-fontColorSecondary"
-                }`}
-              >
-                {/* Display the main image */}
-                <View className="w-full flex items-center mt-2 mb-2">
-                  <Image
-                    source={{ uri: imageData.file_path_heatmap }}
-                    style={{
-                      width: "100%",
-                      maxWidth: screenWidth * 0.9,
-                      height: undefined,
-                      aspectRatio: 1.5,
-                      borderRadius: 10,
-                    }}
-                    resizeMode="contain"
-                  />
-                </View>
-
-                {/* AI Result & Share Button */}
-                <View className="flex flex-row items-center justify-between w-full mt-2 px-2">
-                  {aiPercent !== null && phrase && (
-                    <Text
-                      className={`font-semibold [font-family:'Inter'] text-3xl ${
-                        aiPercent > 0.5 ? "text-white" : "text-backupColor2"
-                      }`}
-                    >
-                      {aiPercent > 0.5 ? "It's AI" : "It's real!"}
-                    </Text>
-                  )}
-                  {aiPercent !== null && phrase && (
-                    <TouchableOpacity
-                      onPress={captureAndShare}
-                      className={`p-3 rounded-lg flex-row justify-center items-center ${
-                        aiPercent > 0.5 ? "bg-backupColor2" : "bg-fontColorPrimary"
-                      }`}
-                    >
-                      <Feather name="share" size={15} color="white" />
-                    </TouchableOpacity>
-                  )}
-                </View>
-
-                {/* Confidence Score */}
-                <View className="w-full px-2">
-                                      {aiPercent !== null && phrase && (
-                                        <Text className="text-2xl font-semibold text-backgroundPrimary [font-family:'Inter'] mt-3">
-                                          {`${aiPercent > 0.5 ? (aiPercent * 100).toFixed(2) : (100 * (1 - aiPercent)).toFixed(2)}% ${phrase}`}
-                                        </Text>
-                                      )}
-                                      <Text className="text-1xl text-backgroundPrimary [font-family:'Inter'] mt-1">
-                                        based on the highlighted areas
-                                      </Text>
-                                    </View>
-
-                {/* Donut Chart */}
-                <View className="flex flex-row items-center mt-1">
-                  {aiPercent !== null && phrase && (
-                    <Donut AI={(aiPercent * 100).toFixed(2)} notAI={(100 - aiPercent * 100).toFixed(2)} />
-                  )}
-                </View>
+        <Animated.View
+          style={{
+            opacity: fadeAnim,
+            transform: [{ translateY: slideAnim }],
+            marginTop: 20,
+          }}
+        >
+          <ViewShot ref={viewShotRef} options={{ format: "png", quality: 0.9 }}>
+            <View
+              className={`flex flex-col items-center justify-between p-4 rounded-lg ${
+                aiPercent > 0.5
+                  ? "bg-fontColorPrimary"
+                  : "bg-fontColorSecondary"
+              }`}
+            >
+              {/* Display the main image */}
+              <View className="w-full flex items-center mt-2 mb-2">
+                <Image
+                  source={{ uri: imageData.file_path_heatmap }}
+                  style={{
+                    width: "100%",
+                    maxWidth: screenWidth * 0.9,
+                    height: undefined,
+                    aspectRatio: 1.5,
+                    borderRadius: 10,
+                  }}
+                  resizeMode="contain"
+                />
               </View>
-            </ViewShot>
-          </Animated.View>
+
+              {/* AI Result & Share Button */}
+              <View className="flex flex-row items-center justify-between w-full mt-2 px-2">
+                {aiPercent !== null && phrase && (
+                  <Text
+                    className={`font-semibold [font-family:'Inter'] text-3xl ${
+                      aiPercent > 0.5 ? "text-white" : "text-backupColor2"
+                    }`}
+                  >
+                    {aiPercent > 0.5 ? "It's AI" : "It's real!"}
+                  </Text>
+                )}
+                {aiPercent !== null && phrase && (
+                  <TouchableOpacity
+                    onPress={captureAndShare}
+                    className={`p-3 rounded-lg flex-row justify-center items-center ${
+                      aiPercent > 0.5
+                        ? "bg-backupColor2"
+                        : "bg-fontColorPrimary"
+                    }`}
+                  >
+                    <Feather name="share" size={15} color="white" />
+                  </TouchableOpacity>
+                )}
+              </View>
+
+              {/* Confidence Score */}
+              <View className="w-full px-2">
+                {aiPercent !== null && phrase && (
+                  <Text className="text-2xl font-semibold text-backgroundPrimary [font-family:'Inter'] mt-3">
+                    {`${
+                      aiPercent > 0.5
+                        ? (aiPercent * 100).toFixed(2)
+                        : (100 * (1 - aiPercent)).toFixed(2)
+                    }% ${phrase}`}
+                  </Text>
+                )}
+                <Text className="text-1xl text-backgroundPrimary [font-family:'Inter'] mt-1">
+                  based on the highlighted areas
+                </Text>
+              </View>
+
+              {/* Donut Chart */}
+              <View className="flex flex-row items-center mt-1">
+                {aiPercent !== null && phrase && (
+                  <Donut
+                    AI={(aiPercent * 100).toFixed(2)}
+                    notAI={(100 - aiPercent * 100).toFixed(2)}
+                  />
+                )}
+              </View>
+            </View>
+          </ViewShot>
+        </Animated.View>
       </View>
     </SafeAreaView>
   );
